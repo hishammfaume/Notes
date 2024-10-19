a337489c0dceec0b3e868033ee621519a2310c84
hishammfaume <hishammfaume@gmail.com>
2024-10-19 17:34:05 +0100

    Fix process exit statement in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,6 +10,6 @@ export const connectDB = async () => {
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     }catch (error){
         console.error(`Error: ${error.message}`);
-        process.exit(1); //exit with failure, 0 means success
+        process.exit(1) //exit with failure, 0 means success
     }
 }
